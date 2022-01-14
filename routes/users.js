430a11138372057584945ff8c00e13cccb54e39c
Fahid RM <fahed@Somewhere.local>
2022-01-14 16:33:28 +0000

    updates

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(
+    [
+      { id: 1, username: 'alice', email: 'alice@company.com', sn: 1},
+      { id: 2, username: 'bob', email: 'alice@company.com', sn: 12}
+    ]
+  );
+});
+
+router.get('/1', function(req, res, next) {
+  res.json({ id: 1, username: 'alice', email: 'alice@company.com', sn: 1});
+});
+
+router.get('/3', function(req, res, next) {
+  res.status(404).json({ message: 'User not found'});
+});
+
+router.post('/', function(req, res, next) {
+  res.status(201).json({ message: 'user created'});
+});
+
+router.put('/1', function(req, res, next) {
+  res.status(200).json({ message: 'User updated'});
+});
+
+router.delete('/1', function(req, res, next) {
+  res.status(200).json({ message: 'User deleted'});
 });
 
 module.exports = router;
